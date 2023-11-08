c7b6375070af5d07830b815c64342a02faeeb6fc
Jone <jone.chong99@gmail.com>
2023-11-08 20:24:22 +0800

    chore: changed single quote to double qoute

--- public/electron.js
+++ public/electron.js
@@ -1,39 +1,38 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
 
-  const startUrl = isDev
-    ? 'http://localhost:3000' // Dev URL
-    : `file://${path.join(__dirname, '../build/index.html')}`; // Prod URL
-  mainWindow.loadURL(startUrl);
+    const startUrl = isDev
+        ? "http://localhost:3000" // Dev URL
+        : `file://${path.join(__dirname, "../build/index.html")}`; // Prod URL
+    mainWindow.loadURL(startUrl);
 
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+    mainWindow.on("closed", function () {
+        mainWindow = null;
+    });
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
+app.on("activate", function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
-
