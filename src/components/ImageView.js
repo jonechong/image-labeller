8f9b376c5b17e736de61b138e5fc145c3768f1f8
Jone <jone.chong99@gmail.com>
2023-11-09 21:54:29 +0800

    feat: styling for ImageView

--- src/components/ImageView.js
+++ src/components/ImageView.js
@@ -1,9 +1,19 @@
-import { Card, CardMedia } from "@mui/material";
+import { Card, CardMedia, Typography } from "@mui/material";
+
 export default function ImageView({ currentImage }) {
     return (
         <>
             {currentImage && (
-                <Card>
+                <Card
+                    sx={{
+                        maxWidth: "100%",
+                        boxShadow: 3, // Adjusts the shadow of the card
+                        border: "1px solid #ccc", // Adds a border
+                        borderRadius: 1, // Adjusts the border radius
+                        margin: "auto", // Centers the card
+                        textAlign: "center",
+                    }}
+                >
                     <CardMedia
                         component="img"
                         image={currentImage}
