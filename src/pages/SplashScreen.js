95de53bf18e7be5836a1f7604ad906e1532c7b40
Jone <jone.chong99@gmail.com>
2023-11-10 12:11:12 +0800

    feat: added spacing between buttons

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -3,12 +3,19 @@ import { useNavigate } from "react-router-dom";
 
 export default function SplashScreen({ onOptionSelect }) {
     const navigate = useNavigate();
+
     return (
-        <Box textAlign="center" p={4}>
-            <Button variant="contained" onClick={() => navigate("download")}>
+        <Box
+            textAlign="center"
+            p={4}
+            display="flex"
+            justifyContent="center"
+            gap={2}
+        >
+            <Button variant="contained" onClick={() => navigate("/download")}>
                 Download Images
             </Button>
-            <Button variant="contained" onClick={() => navigate("label")}>
+            <Button variant="contained" onClick={() => navigate("/label")}>
                 Label Images
             </Button>
         </Box>
