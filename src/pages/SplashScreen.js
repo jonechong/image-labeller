66c2422be882744386a13eea60ef3879c065b041
Jone <jone.chong99@gmail.com>
2023-11-10 11:32:26 +0800

    chore: removed slash

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -3,13 +3,12 @@ import { useNavigate } from "react-router-dom";
 
 export default function SplashScreen({ onOptionSelect }) {
     const navigate = useNavigate();
-
     return (
         <Box textAlign="center" p={4}>
             <Button variant="contained" onClick={() => navigate("download")}>
                 Download Images
             </Button>
-            <Button variant="contained" onClick={() => navigate("/label")}>
+            <Button variant="contained" onClick={() => navigate("label")}>
                 Label Images
             </Button>
         </Box>
