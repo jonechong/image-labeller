6ca854c5b612a4b9ae431bec4f5794d4065c1669
Jone <jone.chong99@gmail.com>
2023-11-10 09:47:31 +0800

    feat: added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 import ImageLabeller from "./pages/ImageLabeller";
+import SplashScreen from "./pages/SplashScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-    return <ImageLabeller />;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<SplashScreen />} />
+                <Route path="/label" element={<ImageLabeller />} />
+                <Route path="/download" element={<ImageDownloader />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
