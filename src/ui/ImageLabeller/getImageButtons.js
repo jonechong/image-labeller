fcb2e3a3b75216dd9f12e72f6c29630445da01c0
Jone <jone.chong99@gmail.com>
2023-11-17 00:15:46 +0800

    feat: added tooltip for ActionButtons

--- src/ui/ImageLabeller/getImageButtons.js
+++ src/ui/ImageLabeller/getImageButtons.js
@@ -7,12 +7,19 @@ export const getImageButtons = (
         label: "Previous Image",
         action: showPrevImage,
         variant: "contained",
+        tooltip: "Press ← arrow key for previous image",
     },
-    { label: "Next Image", action: showNextImage, variant: "contained" },
+    {
+        label: "Next Image",
+        action: showNextImage,
+        variant: "contained",
+        tooltip: "Press → arrow key for next image",
+    },
     {
         label: "Delete Image",
         action: handleDeleteImage,
         variant: "contained",
         color: "secondary",
+        tooltip: "Press Del key to remove image",
     },
 ];
