06ef3dfaaf75008ef585ebbb5508b7b6115b2ffb
Jone <jone.chong99@gmail.com>
2023-11-18 00:51:10 +0800

    feat: renamed and removed cancel button

---
+++ src/ui/ImageDownloader/getDownloadButton.js
@@ -0,0 +1,8 @@
+export const getDownloadButton = (handleSubmit) => [
+    {
+        label: "Submit",
+        action: handleSubmit,
+        variant: "contained",
+        color: "primary",
+    },
+];
