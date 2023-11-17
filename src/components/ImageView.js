ba6003937000d722f326a4b09f039ccffe6bd2a6
Jone <jone.chong99@gmail.com>
2023-11-17 23:17:20 +0800

    feat: consistency with other components by removing the image check

--- src/components/ImageView.js
+++ src/components/ImageView.js
@@ -3,29 +3,27 @@ import { Card, CardMedia } from "@mui/material";
 export default function ImageView({ currentImage }) {
     return (
         <>
-            {currentImage && (
-                <Card
+            <Card
+                sx={{
+                    maxWidth: "100%",
+                    boxShadow: 3, // Adjusts the shadow of the card
+                    border: "1px solid #ccc", // Adds a border
+                    borderRadius: 1, // Adjusts the border radius
+                    margin: "auto", // Centers the card
+                    textAlign: "center",
+                }}
+            >
+                <CardMedia
+                    component="img"
+                    image={currentImage}
+                    alt="Loaded Image"
                     sx={{
+                        maxHeight: 500,
                         maxWidth: "100%",
-                        boxShadow: 3, // Adjusts the shadow of the card
-                        border: "1px solid #ccc", // Adds a border
-                        borderRadius: 1, // Adjusts the border radius
-                        margin: "auto", // Centers the card
-                        textAlign: "center",
+                        objectFit: "contain",
                     }}
-                >
-                    <CardMedia
-                        component="img"
-                        image={currentImage}
-                        alt="Loaded Image"
-                        sx={{
-                            maxHeight: 500,
-                            maxWidth: "100%",
-                            objectFit: "contain",
-                        }}
-                    />
-                </Card>
-            )}
+                />
+            </Card>
         </>
     );
 }
