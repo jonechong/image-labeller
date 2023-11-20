27110ca3f2a69c5bd9af7e44f63f06020ec91aa9
Jone <jone.chong99@gmail.com>
2023-11-20 23:13:26 +0800

    feat: dummy bounding box

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import SplashScreen from "./pages/SplashScreen";
 import ImageLabeller from "./pages/ImageLabeller";
 import ImageDownloader from "./pages/ImageDownloader";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BoundingBoxDrawer from "./components/ImageLabeller/BoundingBox";
 
 function App() {
+    const handleAnnotationComplete = (rectangles) => {
+        console.log("Bounding Box Annotations:", rectangles);
+    };
+
     return (
-        <Router style={{ display: "flex", flex: 1 }}>
-            <Routes>
-                <Route path="/" element={<SplashScreen />} />
-                <Route path="/label" element={<ImageLabeller />} />
-                <Route path="/download" element={<ImageDownloader />} />
-            </Routes>
-        </Router>
+        <div style={{ padding: 100 }}>
+            <BoundingBoxDrawer
+                imageUrl="path_to_your_image.jpg"
+                onAnnotationComplete={handleAnnotationComplete}
+            />
+        </div>
     );
+    // return (
+    //     <Router style={{ display: "flex", flex: 1 }}>
+    //         <Routes>
+    //             <Route path="/" element={<SplashScreen />} />
+    //             <Route path="/label" element={<ImageLabeller />} />
+    //             <Route path="/download" element={<ImageDownloader />} />
+    //         </Routes>
+    //     </Router>
+    // );
 }
 
 export default App;
