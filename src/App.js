1762c058d386ccf276791ed81a3c50162a61a6dd
Jone <jone.chong99@gmail.com>
2023-11-22 10:56:13 +0800

    feat: added bounding boxes feature to ImageView component

--- src/App.js
+++ src/App.js
@@ -3,30 +3,17 @@ import SplashScreen from "./pages/SplashScreen";
 import ImageLabeller from "./pages/ImageLabeller";
 import ImageDownloader from "./pages/ImageDownloader";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import BoundingBoxDrawer from "./components/ImageLabeller/BoundingBox";
 
 function App() {
-    const handleAnnotationComplete = (rectangles) => {
-        console.log("Bounding Box Annotations:", rectangles);
-    };
-
     return (
-        <div style={{ padding: 100 }}>
-            <BoundingBoxDrawer
-                imageUrl="path_to_your_image.jpg"
-                onAnnotationComplete={handleAnnotationComplete}
-            />
-        </div>
+        <Router style={{ display: "flex", flex: 1 }}>
+            <Routes>
+                <Route path="/" element={<SplashScreen />} />
+                <Route path="/label" element={<ImageLabeller />} />
+                <Route path="/download" element={<ImageDownloader />} />
+            </Routes>
+        </Router>
     );
-    // return (
-    //     <Router style={{ display: "flex", flex: 1 }}>
-    //         <Routes>
-    //             <Route path="/" element={<SplashScreen />} />
-    //             <Route path="/label" element={<ImageLabeller />} />
-    //             <Route path="/download" element={<ImageDownloader />} />
-    //         </Routes>
-    //     </Router>
-    // );
 }
 
 export default App;
--- src/components/ImageView.js
+++ src/components/ImageView.js
@@ -1,30 +1,146 @@
-import { Card, CardMedia } from "@mui/material";
+import React, { useState, useEffect, useRef } from "react";
+import { Stage, Layer, Image, Rect } from "react-konva";
+import useImage from "use-image";
+import { Card } from "@mui/material";
 
 export default function ImageView({ currentImage }) {
+    const [image] = useImage(currentImage);
+    const [boxes, setBoxes] = useState([]);
+    const [newBox, setNewBox] = useState(null);
+    const [stageSize, setStageSize] = useState({ width: 0, height: 0 });
+    const cardRef = useRef(null);
+
+    const createNewBox = () => {
+        if (newBox) {
+            setBoxes([...boxes, newBox]);
+            setNewBox(null);
+        }
+    };
+
+    const undoLastBox = () => {
+        setBoxes(boxes.slice(0, -1)); // Removes the last box
+    };
+
+    // mouse handlers
+    const handleMouseDown = (e) => {
+        if (e.evt.button === 0) {
+            // Ensure it's the left mouse button
+            const { x, y } = e.target.getStage().getPointerPosition();
+            setNewBox({ x, y, width: 0, height: 0 });
+        }
+    };
+
+    const handleMouseMove = (e) => {
+        if (!newBox) {
+            return;
+        }
+        const stage = e.target.getStage();
+        const { x, y } = stage.getPointerPosition();
+        const updatedBox = {
+            ...newBox,
+            width: x - newBox.x,
+            height: y - newBox.y,
+        };
+        setNewBox(updatedBox);
+    };
+
+    const handleRightClick = (e) => {
+        e.evt.preventDefault(); // Prevents default right-click menu
+        undoLastBox();
+        console.log(boxes);
+    };
+
+    // useEffects
+    useEffect(() => {
+        // Function to update stage size based on the card size and image aspect ratio
+        const updateSize = () => {
+            if (cardRef.current && image) {
+                const cardWidth = cardRef.current.offsetWidth;
+                const cardHeight = cardRef.current.offsetHeight;
+                const aspectRatio = image.width / image.height;
+
+                let newWidth, newHeight;
+                if (cardWidth / cardHeight > aspectRatio) {
+                    // Card is wider than image
+                    newHeight = cardHeight;
+                    newWidth = cardHeight * aspectRatio;
+                } else {
+                    // Card is taller than image
+                    newWidth = cardWidth;
+                    newHeight = cardWidth / aspectRatio;
+                }
+
+                setStageSize({ width: newWidth, height: newHeight });
+            }
+        };
+
+        window.addEventListener("resize", updateSize);
+        updateSize();
+
+        return () => window.removeEventListener("resize", updateSize);
+    }, [image]);
+
     return (
-        <>
-            <Card
-                sx={{
-                    width: "100%",
-                    boxShadow: 3, // Adjusts the shadow of the card
-                    border: "1px solid #ccc", // Adds a border
-                    borderRadius: 1, // Adjusts the border radius
-                    margin: "auto", // Centers the card
-                    textAlign: "center",
-                    height: "60vh",
-                }}
+        <Card
+            ref={cardRef}
+            sx={{
+                width: "100%",
+                boxShadow: 3,
+                border: "1px solid #ccc",
+                borderRadius: 1,
+                margin: "auto",
+                textAlign: "center",
+                height: "60vh",
+                position: "relative",
+                overflow: "hidden",
+            }}
+        >
+            <Stage
+                width={stageSize.width}
+                height={stageSize.height}
+                onMouseDown={handleMouseDown}
+                onMouseMove={handleMouseMove}
+                onMouseUp={createNewBox}
+                onMouseLeave={createNewBox}
+                onContextMenu={handleRightClick}
+                style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                }} // Center the stage
             >
-                <CardMedia
-                    component="img"
-                    image={currentImage}
-                    alt="Loaded Image"
-                    sx={{
-                        maxHeight: "100%",
-                        maxWidth: "100%",
-                        objectFit: "contain",
-                    }}
-                />
-            </Card>
-        </>
+                <Layer>
+                    <Image
+                        image={image}
+                        width={stageSize.width}
+                        height={stageSize.height}
+                    />
+                    {boxes.map((box, i) => (
+                        <Rect
+                            key={i}
+                            x={box.x}
+                            y={box.y}
+                            width={box.width}
+                            height={box.height}
+                            fillEnabled={false}
+                            stroke="red"
+                            strokeWidth={2}
+                        />
+                    ))}
+                    {newBox && (
+                        <Rect
+                            x={newBox.x}
+                            y={newBox.y}
+                            width={newBox.width}
+                            height={newBox.height}
+                            fillEnabled={false}
+                            stroke="red"
+                            strokeWidth={2}
+                        />
+                    )}
+                </Layer>
+            </Stage>
+        </Card>
     );
 }
